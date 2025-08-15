af17ad776a2fe28b2e1f5c2bbb2927e958e85689
선빈 <ehtjsqls@gmail.com>
2025-08-15 15:50:18 +0900

    feat: LiquorQueryRepository 추가 및 주류 조회 기능 구현

---
+++ src/main/kotlin/com/zzan/zzan/liquor/query/repository/LiquorQueryRepository.kt
@@ -0,0 +1,33 @@
+package com.zzan.zzan.liquor.query.repository
+
+import com.zzan.zzan.api.liquor.dto.LiquorDetailResponse
+import com.zzan.zzan.liquor.command.domain.Liquor
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+
+
+@Repository
+interface LiquorQueryRepository : JpaRepository<Liquor, String> {
+
+    @Query(
+        nativeQuery = true,
+        value = """
+            SELECT
+                l.id,
+                l.name,
+                l.type,
+                l.description,
+                l.food_pairing,
+                l.volume,
+                l.content,
+                l.awards,
+                l.etc,
+                l.image_url,
+                l.brewery
+            FROM liquors l
+            WHERE l.id = :id
+        """,
+    )
+    fun getLiquorById(id: String): LiquorDetailResponse?
+}
