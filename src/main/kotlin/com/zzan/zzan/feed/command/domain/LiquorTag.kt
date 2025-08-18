5e396a95eb9730c6ff1d94127db3fdf6b39049c9
선빈 <ehtjsqls@gmail.com>
2025-08-18 18:30:18 +0900

    feat: LiquorTag 엔티티에 liquorName 및 평점 필드 추가, 태그 위치 필드 수정

--- src/main/kotlin/com/zzan/zzan/feed/command/domain/LiquorTag.kt
+++ src/main/kotlin/com/zzan/zzan/feed/command/domain/LiquorTag.kt
@@ -8,7 +8,7 @@ import jakarta.persistence.Table
 
 @Entity
 @Table(name = "liquor_tags")
-class LiquorTag (
+class LiquorTag(
     @Id
     @Column(length = 26)
     val id: String = UlidCreator.getUlid().toString(),
@@ -16,15 +16,20 @@ class LiquorTag (
     @Column(name = "liquor_id", length = 26)
     val liquorId: String, // 전통주 ID
 
+    @Column(name = "liquor_name", length = 50)
+    val liquorName: String, // 전통주 이름
+
     @Column(name = "image_id", length = 26)
-    val imageId: String, // 이미지 ID
+    val imageId: String, // 태그가 달린 이미지 ID
 
     @Column(name = "feed_id", length = 26)
     val feedId: String, // 태그 ID
 
-    @Column(precision = 10, scale = 6)
-    val x: Double, // 태그 위치 X 좌표 (0~1 범위 상대좌표)
+    val score: Double? = null, // 전통주 평점 (0~5)
 
-    @Column(precision = 10, scale = 6)
-    val y: Double // 태그 위치 Y 좌표 (0~1 범위 상대좌표)
+    @Column(precision = 10)
+    val tagX: Double, // 태그 위치 X 좌표 (0~1 범위 상대좌표)
+
+    @Column(precision = 10)
+    val tagY: Double, // 태그 위치 Y 좌표 (0~1 범위 상대좌표)
 )
