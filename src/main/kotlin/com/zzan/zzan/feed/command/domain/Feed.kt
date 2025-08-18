549ff08d56326928f97b2c41cdfb278b3a56012c
선빈 <ehtjsqls@gmail.com>
2025-08-18 18:31:50 +0900

    feat: Feed 엔티티의 필드 수정 및 AuditingEntityListener 추가

--- src/main/kotlin/com/zzan/zzan/feed/command/domain/Feed.kt
+++ src/main/kotlin/com/zzan/zzan/feed/command/domain/Feed.kt
@@ -1,12 +1,14 @@
 package com.zzan.zzan.feed.command.domain
 
+import com.github.f4b6a3.ulid.UlidCreator
 import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
-import com.github.f4b6a3.ulid.UlidCreator
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @Entity
 @Table(name = "feeds")
+@EntityListeners(AuditingEntityListener::class)
 class Feed(
     @Id
     @Column(length = 26)
@@ -18,20 +20,20 @@ class Feed(
     @Column(name = "image_url")
     val imageUrl: String, // 피드 대표 이미지 URL
 
-    val star: Int = 0, // 피드 평점 (0~5)
+    val score: Double? = null, // 피드 평점 (0~5)
 
     @Column(columnDefinition = "TEXT")
-    val text: String, // 피드 내용
+    val text: String? = null, // 피드 내용
 
     @Column(name = "place_id", length = 26)
-    val placeId: String?, // 피드 장소 ID
+    val placeId: String, // 피드 장소 ID
 
     @Column(name = "buy_place_id", length = 26)
-    val buyPlaceId : String? = null, // 전통주 구매 장소 ID
+    val buyPlaceId: String? = null, // 전통주 구매 장소 ID
 
     @CreatedDate
     @Column(name = "created_at")
-    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val createdAt: LocalDateTime? = null,
 
     @Column(name = "deleted_at")
     val deletedAt: LocalDateTime? = null
