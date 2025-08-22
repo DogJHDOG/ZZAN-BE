5e165623c2c57c9ccd6bad693290b9bf1d5bad2e
선빈 <ehtjsqls@gmail.com>
2025-08-23 00:45:38 +0900

    style: 가독성을 위한 Places 탐색 쿼리 포맷팅 변경

--- src/main/kotlin/com/zzan/zzan/place/query/repository/PlaceQueryRepository.kt
+++ src/main/kotlin/com/zzan/zzan/place/query/repository/PlaceQueryRepository.kt
@@ -10,15 +10,20 @@ import org.springframework.data.repository.query.Param
 interface PlaceQueryRepository : JpaRepository<Place, Long> {
     @Query(
         """
-        SELECT new com.zzan.zzan.api.place.dto.PlaceResponse(
-            p.id, p.name, p.count, p.score, 
-            p.address, p.phone, p.longitude, p.latitude
-        )
-        FROM Place p 
-        WHERE function('ST_Within', p.location, 
-               function('ST_MakeEnvelope', :#{#viewBox.minLongitude.value}, :#{#viewBox.minLatitude.value}, 
-                                          :#{#viewBox.maxLongitude.value}, :#{#viewBox.maxLatitude.value}, 4326)) = true
-    """
+            SELECT new com.zzan.zzan.api.place.dto.PlaceResponse(
+                p.id, p.name, p.count, p.score, 
+                p.address, p.phone, p.longitude, p.latitude
+            )
+            FROM Place p 
+            WHERE function('ST_Within', p.location, 
+                   function('ST_MakeEnvelope', 
+                       :#{#viewBox.minLongitude.value}, 
+                       :#{#viewBox.minLatitude.value}, 
+                       :#{#viewBox.maxLongitude.value}, 
+                       :#{#viewBox.maxLatitude.value}, 
+                       4326)) = true
+        """
     )
     fun findPlacesByViewBox(@Param("viewBox") viewBox: ViewBox): List<PlaceResponse>
 }
+
