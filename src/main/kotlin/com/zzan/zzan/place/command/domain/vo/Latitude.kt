7139f07b69b47c434a1a298c9b491d0258b37030
선빈 <ehtjsqls@gmail.com>
2025-08-23 00:35:01 +0900

    fix: JPA 호환성을 위한 value class -> data class 변경
    - Longitude.kt data class로 변경
    - Latitude.kt data class로 변경

--- src/main/kotlin/com/zzan/zzan/place/command/domain/vo/Latitude.kt
+++ src/main/kotlin/com/zzan/zzan/place/command/domain/vo/Latitude.kt
@@ -1,7 +1,6 @@
 package com.zzan.zzan.place.command.domain.vo
 
-@JvmInline
-value class Latitude(val value: Double) {
+data class Latitude(val value: Double) {
     init {
         require(value in -90.0..90.0) { "위도는 -90도에서 90도 사이여야 합니다: $value" }
     }
--- src/main/kotlin/com/zzan/zzan/place/command/domain/vo/Longitude.kt
+++ src/main/kotlin/com/zzan/zzan/place/command/domain/vo/Longitude.kt
@@ -1,7 +1,6 @@
 package com.zzan.zzan.place.command.domain.vo
 
-@JvmInline
-value class Longitude(val value: Double) {
+data class Longitude(val value: Double) {
     init {
         require(value in -180.0..180.0) { "경도는 -180도에서 180도 사이의 값이어야 합니다: $value" }
     }
